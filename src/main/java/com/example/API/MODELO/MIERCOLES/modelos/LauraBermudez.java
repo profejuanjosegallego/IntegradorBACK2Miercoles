607e16bba3e9968082eec92ff86d41711e557ed4
LauBermudez <laurabermudezp04@gmail.com>
2025-08-27 08:29:46 -0500

    practicandooo

--- src/main/java/com/example/API/MODELO/MIERCOLES/modelos/LauraBermudez.java
+++ src/main/java/com/example/API/MODELO/MIERCOLES/modelos/LauraBermudez.java
@@ -12,21 +12,29 @@ public class LauraBermudez {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
+    @Column(name = "fullName")
+    private String fullName;
+
     @Column(name = "adress", length = 80)
     private String adress;
 
     @Column(name = "isHincha")
     private Boolean isVerdolaga;
 
+    @Column(name = "favoriteAnimal")
+    private String favoriteAnimal;
+
     // constructors
 
     public LauraBermudez() {
     }
 
-    public LauraBermudez(String adress, Boolean isVerdolaga, Integer id) {
+    public LauraBermudez(String adress, Boolean isVerdolaga, Integer id, String fullName, String favoriteAnimal) {
         this.adress = adress;
         this.isVerdolaga = isVerdolaga;
         this.id = id;
+        this.fullName = fullName;
+        this.favoriteAnimal = favoriteAnimal;
     }
 
     // getters
@@ -43,6 +51,14 @@ public class LauraBermudez {
         return id;
     }
 
+    public String getFullName() {
+        return fullName;
+    }
+
+    public String getFavoriteAnimal() {
+        return favoriteAnimal;
+    }
+
     // setters
 
     public void setAdress(String adress) {
@@ -55,5 +71,13 @@ public class LauraBermudez {
 
     public void setId(Integer id) {
         this.id = id;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public void setFavoriteAnimal(String favoriteAnimal) {
+        this.favoriteAnimal = favoriteAnimal;
     }
 }
